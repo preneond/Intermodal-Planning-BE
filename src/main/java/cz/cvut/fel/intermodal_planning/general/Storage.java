5ddcc8647951be16d9a4d3cd536924fe66f3778c
Ondrej Prenek <ondrej.prenek@zentity.com>
2018-05-09 14:25:12 +0200

    changed project root path

--- src/main/java/cz/cvut/fel/intermodal_planning/general/Storage.java
+++ src/main/java/cz/cvut/fel/intermodal_planning/general/Storage.java
@@ -8,7 +8,7 @@ import java.net.URL;
 public final class Storage {
     public static URL GRAPH_RESOURCE = Storage.class.getResource("/graph.json");
 
-    private static final String ROOT_PATH = "/Users/ondrejprenek/Documents/CVUT/Bachelor_thesis/Intermodal_planning/";
+    private static final String ROOT_PATH = "/Users/ondrejprenek/Documents/CVUT/Bachelor_thesis/source/";
 
     public static String DATA_PATH = ROOT_PATH + "Data/";
     public static String STATISTICS_PATH = DATA_PATH + "statistics/";
@@ -74,68 +74,67 @@ public final class Storage {
             "--------------------------------------------------------------------------------";
 
     public static String[] GMAPS_API_KEYS = new String[]{
-//            "AIzaSyDPlXXLGlWdjqyha8M5IWJMfgM4kf_uV4A",
-//            "AIzaSyBAJwyMaIhPdMptdXbDcFYGzl-86J2oyKw",
-//            "AIzaSyB1BXePutkWKhuKzk3TTTFKvjtaDKslI5A",
-//            "AIzaSyDqe-iuB8J9H4zUwZ1APCYzayuOx7-oKgg",
-//            "AIzaSyBdF2b3EpPazT5NPl_xRlcWXpBH4Pt0GWE",
-//            "AIzaSyDePcOSehCxz5SCFsDMPVqlUHq0BMIrTx4",
-//            "AIzaSyBsYq8wlQIlEHu0XpSWR-u1pbRJogKwdiQ",
-//            "AIzaSyDTcjYJ339Hah37BlxIJcAacKGlBwMwuhQ",
-//            "AIzaSyDZXEPEEpBf6dIP1mObKAfemBODucsUDzQ",
-//            "AIzaSyDbn9PkZ1UHDzi-P1h57-mo-endxen87xU",
-//
-//            "AIzaSyCZV9E5HbNR4CjrlEp_jMOJURL2BicG_YA",
-//            "AIzaSyDyPrdAcyz3LDVizBf8jquz1Vin9Y4Z9EI",
-//            "AIzaSyAXNikg6MUwdMpyYnv262tFPe_24HDpa2c",
-//            "AIzaSyDcfkhEBRsksDgV6wXMaEuo5JcamTiC4vE",
-//            "AIzaSyAcyZTi6cZhzPBbDrYQvkSHrxcmb1xmGic",
-//            "AIzaSyBjzfYZnQ-06T0PvBOyQciJ_T4V8_N_fVM",
-//            "AIzaSyB9q1BRhBx1bGvIWUU8XS9O3084pfcKT0U",
-//            "AIzaSyCIFkOUztayI2WM2-MW0-BHz29UWx5nImA",
-//            "AIzaSyDDWhg7lvEcbVh-wBkdQiPLeItnh8pEkbI",
-//            "AIzaSyBXRDozUF7nHfYmsNLhp67XqxFQbq4aJHQ",
-//            "AIzaSyCZPYKlhHT5FDxD865JM52oM2CC1Q_x-i4",
-//
-//            "AIzaSyCGozoXcxaqvftw2uMIEEfrvLnGmvejV_g",
-//            "AIzaSyDc3e6AqFuoLo3CFJlTmd9Uh13gJffyoJY",
-//            "AIzaSyDy2NG8bbS9XWohxVO_3BCCnqxmb7cwzwg",
-//            "AIzaSyCr0-ICL2e7wiKme8NZBGN1PlN2-X5YMxk",
-//            "AIzaSyCx24szrOOBrY5MOCPV5_riS9ormoLvVSM",
-//            "AIzaSyBIOcxaLyVSn5yA8FjEusHst8ig1Ql6Kf8",
-//            "AIzaSyCPnugKTNY-RgXInnuSU2qM7b3GUUuKGVU",
-//            "AIzaSyDQyn032gKwSJrk94Xh7GUXlcKhBDrYCu8",
-//            "AIzaSyDnydUcFe15dC8iuKGL9RsRG39gNWQnsh0",
-//            "AIzaSyBnr2a2o2wGe0GDrxpkJ4Vs893I6oejjNM",
-//            "AIzaSyCIdROtkPV7yfR9diMsbr0XlSMVaMNTYzU"
+            "AIzaSyDPlXXLGlWdjqyha8M5IWJMfgM4kf_uV4A",
+            "AIzaSyBAJwyMaIhPdMptdXbDcFYGzl-86J2oyKw",
+            "AIzaSyB1BXePutkWKhuKzk3TTTFKvjtaDKslI5A",
+            "AIzaSyDqe-iuB8J9H4zUwZ1APCYzayuOx7-oKgg",
+            "AIzaSyBdF2b3EpPazT5NPl_xRlcWXpBH4Pt0GWE",
+            "AIzaSyDePcOSehCxz5SCFsDMPVqlUHq0BMIrTx4",
+            "AIzaSyBsYq8wlQIlEHu0XpSWR-u1pbRJogKwdiQ",
+            "AIzaSyDTcjYJ339Hah37BlxIJcAacKGlBwMwuhQ",
+            "AIzaSyDZXEPEEpBf6dIP1mObKAfemBODucsUDzQ",
+            "AIzaSyDbn9PkZ1UHDzi-P1h57-mo-endxen87xU",
 
-//            "AIzaSyAUgv7J0Zh0kYi6jLVhU_EMBbk0BaSUPsc",
-//            "AIzaSyCerD_3Kps7awYuvXbgJ4bQ6BmBTuH83bo",
-//            "AIzaSyDuKzxMVJc6wVhiMCVphzpEmwOyjM9eH3A",
-//            "AIzaSyCBlVhbXU05RSHqB3SQx8keCvBsK_pYnzA",
-//            "AIzaSyCvLXUi0EmhgydOR-bGUKdbrBS7CUsvR-k",
-//            "AIzaSyDj8PJmYGCUMz2Drydw5-FxIiSJaz7Ey-o",
-//            "AIzaSyBphBA-fk7lLBUjovENUrSk2iIuJInCw8Y",
-//            "AIzaSyD4RswjmwCvxc4K9rzG06YhbOCpPPAoYPw",
-//            "AIzaSyACgTqrn7Ex-5-za7MJdxxbrvBviFkDWH4",
-//            "AIzaSyDIJd9oDd_PWo2zJy5uPbtv8K7JtbIJ_1E",
-//
-//            "AIzaSyAhijbAS1x_34zlSRROCUsyBn_HnwKgdC8",
-//            "AIzaSyBuGjuqsTJPrm8nVt0qmkeCV1jKTDCW8rE",
-//            "AIzaSyC5ITnvkeAywyfBVzrV842g3k7JQnqVj24",
-//            "AIzaSyDwRmiuYaiHAW2Bh7JW7GbOFN7bHUErrN8",
-//            "AIzaSyCSOr2ZzJad2qs61ANnYMZfVVMguoQdQhA",
-//            "AIzaSyDneayG-UaftJPtg7oDGIUPh-b20o1sg3Q",
-//            "AIzaSyBiA3aRZtosp3Aecz0hmc1LQ9kRxKliXRE",
-//            "AIzaSyDjc9PkcUpLDzn42VoTwIfnhnVx3a06zcI",
-//            "AIzaSyARFWGY5lB_yjVBKjEoUulOotVpu2Zp2oY",
-//            "AIzaSyDKoUMzKbBtGY5owYibjydukmOdilksm-M",
-//            "AIzaSyCL60GZMTlJZeKGLtaZQrqAJ_W2XpDRRmA",
-//
-//            "AIzaSyB-Zi49hIZvnQ9gYSu17HOma5dsJJS5lzU",
-//            "AIzaSyCiIM6jXU85muW1vh9WVpyZ5QVUuc29a_8",
-//            "AIzaSyDgxHPZ10hiSN3Lu8MPyUP_JMn4cUsN5s0"
+            "AIzaSyCZV9E5HbNR4CjrlEp_jMOJURL2BicG_YA",
+            "AIzaSyDyPrdAcyz3LDVizBf8jquz1Vin9Y4Z9EI",
+            "AIzaSyAXNikg6MUwdMpyYnv262tFPe_24HDpa2c",
+            "AIzaSyDcfkhEBRsksDgV6wXMaEuo5JcamTiC4vE",
+            "AIzaSyAcyZTi6cZhzPBbDrYQvkSHrxcmb1xmGic",
+            "AIzaSyBjzfYZnQ-06T0PvBOyQciJ_T4V8_N_fVM",
+            "AIzaSyB9q1BRhBx1bGvIWUU8XS9O3084pfcKT0U",
+            "AIzaSyCIFkOUztayI2WM2-MW0-BHz29UWx5nImA",
+            "AIzaSyDDWhg7lvEcbVh-wBkdQiPLeItnh8pEkbI",
+            "AIzaSyBXRDozUF7nHfYmsNLhp67XqxFQbq4aJHQ",
+            "AIzaSyCZPYKlhHT5FDxD865JM52oM2CC1Q_x-i4",
 
+            "AIzaSyCGozoXcxaqvftw2uMIEEfrvLnGmvejV_g",
+            "AIzaSyDc3e6AqFuoLo3CFJlTmd9Uh13gJffyoJY",
+            "AIzaSyDy2NG8bbS9XWohxVO_3BCCnqxmb7cwzwg",
+            "AIzaSyCr0-ICL2e7wiKme8NZBGN1PlN2-X5YMxk",
+            "AIzaSyCx24szrOOBrY5MOCPV5_riS9ormoLvVSM",
+            "AIzaSyBIOcxaLyVSn5yA8FjEusHst8ig1Ql6Kf8",
+            "AIzaSyCPnugKTNY-RgXInnuSU2qM7b3GUUuKGVU",
+            "AIzaSyDQyn032gKwSJrk94Xh7GUXlcKhBDrYCu8",
+            "AIzaSyDnydUcFe15dC8iuKGL9RsRG39gNWQnsh0",
+            "AIzaSyBnr2a2o2wGe0GDrxpkJ4Vs893I6oejjNM",
+            "AIzaSyCIdROtkPV7yfR9diMsbr0XlSMVaMNTYzU",
+
+            "AIzaSyAUgv7J0Zh0kYi6jLVhU_EMBbk0BaSUPsc",
+            "AIzaSyCerD_3Kps7awYuvXbgJ4bQ6BmBTuH83bo",
+            "AIzaSyDuKzxMVJc6wVhiMCVphzpEmwOyjM9eH3A",
+            "AIzaSyCBlVhbXU05RSHqB3SQx8keCvBsK_pYnzA",
+            "AIzaSyCvLXUi0EmhgydOR-bGUKdbrBS7CUsvR-k",
+            "AIzaSyDj8PJmYGCUMz2Drydw5-FxIiSJaz7Ey-o",
+            "AIzaSyBphBA-fk7lLBUjovENUrSk2iIuJInCw8Y",
+            "AIzaSyD4RswjmwCvxc4K9rzG06YhbOCpPPAoYPw",
+            "AIzaSyACgTqrn7Ex-5-za7MJdxxbrvBviFkDWH4",
+            "AIzaSyDIJd9oDd_PWo2zJy5uPbtv8K7JtbIJ_1E",
+
+            "AIzaSyAhijbAS1x_34zlSRROCUsyBn_HnwKgdC8",
+            "AIzaSyBuGjuqsTJPrm8nVt0qmkeCV1jKTDCW8rE",
+            "AIzaSyC5ITnvkeAywyfBVzrV842g3k7JQnqVj24",
+            "AIzaSyDwRmiuYaiHAW2Bh7JW7GbOFN7bHUErrN8",
+            "AIzaSyCSOr2ZzJad2qs61ANnYMZfVVMguoQdQhA",
+            "AIzaSyDneayG-UaftJPtg7oDGIUPh-b20o1sg3Q",
+            "AIzaSyBiA3aRZtosp3Aecz0hmc1LQ9kRxKliXRE",
+            "AIzaSyDjc9PkcUpLDzn42VoTwIfnhnVx3a06zcI",
+            "AIzaSyARFWGY5lB_yjVBKjEoUulOotVpu2Zp2oY",
+            "AIzaSyDKoUMzKbBtGY5owYibjydukmOdilksm-M",
+            "AIzaSyCL60GZMTlJZeKGLtaZQrqAJ_W2XpDRRmA",
+
+            "AIzaSyB-Zi49hIZvnQ9gYSu17HOma5dsJJS5lzU",
+            "AIzaSyCiIM6jXU85muW1vh9WVpyZ5QVUuc29a_8",
+            "AIzaSyDgxHPZ10hiSN3Lu8MPyUP_JMn4cUsN5s0",
             "AIzaSyDIDmCHkbnLARfRxCQAUZqLa9z_PMAf9Ys",
             "AIzaSyA3LPPwIqplugfQDg2Z-YInpitJ2I6zbws",
             "AIzaSyAZkJoo7GUviUBnrozoxmR86hLc0jNaynI",
@@ -175,9 +174,6 @@ public final class Storage {
             "AIzaSyBGKOvcAreW-Y1XRLHYFQjrqr6Garty7M8",
             "AIzaSyAOyCO1XDVo_ReqES7agVujAFmZRssz3a0",
             "AIzaSyCaT94Ddne03nXwi854KplqN-qfy7eRoRk"
-
-
-
     };
     public static final int FINDING_PATH_COUNT = 100;
 
